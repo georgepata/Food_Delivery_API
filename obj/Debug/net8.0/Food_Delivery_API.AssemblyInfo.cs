82de25176b73ac5736868f13e614d5ecd33f3532
georgepata <geo.patza@yahoo.com>
2024-05-10 14:15:14 +0300

    Deleting Authority Folder

--- obj/Debug/net8.0/Food_Delivery_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Food_Delivery_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Food_Delivery_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f3111f1aa700ef2ce5f92570710756a8716765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a0c54528ce7ec02f01b0c6ef66c5646b2fd38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Food_Delivery_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Food_Delivery_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
